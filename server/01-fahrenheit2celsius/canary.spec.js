ddd416c47f6e58f630eb250cf3a620a87660219b
WIL PANNELL <wgpannel@us.ibm.com>
2016-12-03 21:39:21 -0800

    fail fahrenheit2celsius test

--- server/01-fahrenheit2celsius/canary.spec.js
+++ server/01-fahrenheit2celsius/canary.spec.js
@@ -5,6 +5,9 @@ describe('the fahrenheit2celsius canary test', () => {
 });
 
 describe('fahrenheit2celsius should', () => {
-  it('give 0°C for 32°F');
+  it('give 0°C for 32°F', () => {
+    const expected = fahrenheit2celsius(32);
+    expected.should.equal(0);
+  });
   it('give 10°C for 50°F');
 });
