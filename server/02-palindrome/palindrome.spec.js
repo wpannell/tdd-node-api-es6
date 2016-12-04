81b6beac834ad0099a2c570eca563e11ed5e96de
WIL PANNELL <wgpannel@us.ibm.com>
2016-12-04 00:11:29 -0800

    design test list for palindrome

---
+++ server/02-palindrome/palindrome.spec.js
@@ -0,0 +1,15 @@
+describe('the palindrome canary test', () => {
+  it('shows the infrastructure works', () => {
+    true.should.equal(true);
+  });
+
+  describe('palindrome should be', () => {
+    it('yes for mom');
+    it('yes for dad');
+    it('no for dude');
+    it('yes for mom mom');
+    it('no for mom dad');
+    it('no for empty phrase');
+    it('no for whitespace only phrase');
+  });
+});
