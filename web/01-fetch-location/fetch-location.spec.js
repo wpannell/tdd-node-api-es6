316558cb67c29e7ce2c562feb5de0f6c26da376e
WIL PANNELL <wgpannel@us.ibm.com>
2016-12-04 05:15:18 -0800

    design test list for fetch location

---
+++ web/01-fetch-location/fetch-location.spec.js
@@ -0,0 +1,9 @@
+describe('the fetch-location canary test', () => {
+  it('shows the infrastructure works', () => {
+    true.should.equal(true);
+  });
+
+  describe('fetch location should', () => {
+    it('retrieve the latitude and longitude');
+  });
+});
