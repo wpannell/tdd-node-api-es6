426e4cc2c11efc7baa47370a8401906a48bc8b86
WIL PANNELL <wgpannel@us.ibm.com>
2016-12-05 10:31:02 -0800

    design test list for line count with promise

---
+++ server/04-line-count-with-promise/line-count.spec.js
@@ -0,0 +1,13 @@
+describe('the line-count-with-promise canary test', () => {
+  it('shows the infrastructure works', () => {
+    true.should.equal(true);
+  });
+
+  describe('line count can be checked', () => {
+    it('with the done parameter like with callback');
+
+    it('by returning the promise after .then');
+
+    it('by using eventually or fulfilledWith');
+  });
+});
