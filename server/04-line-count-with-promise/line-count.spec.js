9875f6180372e579faef152e1d04ee4fa2571994
WIL PANNELL <wgpannel@us.ibm.com>
2016-12-06 02:52:13 -0800

    make test fail

--- server/04-line-count-with-promise/line-count.spec.js
+++ server/04-line-count-with-promise/line-count.spec.js
@@ -1,10 +1,20 @@
+import {lineCount} from './line-count';
+
 describe('the line-count-with-promise canary test', () => {
   it('shows the infrastructure works', () => {
     true.should.equal(true);
   });
 
   describe('line count can be checked', () => {
-    it('with the done parameter like with callback');
+    const filespec = 'server/03-line-count-with-callback/line-count.js';
+
+    it('with the done parameter like with callback', done => {
+      const receiveCount = count => {
+        count.should.equal(13);
+      };
+
+      lineCount(filespec).then(receiveCount);
+    });
 
     it('by returning the promise after .then');
 
